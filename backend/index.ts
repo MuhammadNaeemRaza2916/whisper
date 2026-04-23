1b5d75d885e31191471bf0b26e66dadabd3666b2
MuhammadNaeemRaza2916 <muhammadnaeemraza16@gmail.com>
2026-04-23 09:01:32 +0500

    feat: implement socket server initialization and user authentication

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,21 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket";
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
+
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is up and running on port ${PORT}`);
-    })
-  }).catch((error) => {
-    console.error("Failed to connect to the database:", error);
+    httpServer.listen(PORT, () => {
+      console.log("Server is running on PORT:", PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server:", error);
     process.exit(1);
   });
