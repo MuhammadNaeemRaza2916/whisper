5d03e46ab1bc6845884bada49e0cfd47693fb4f1
MuhammadNaeemRaza2916 <muhammadnaeemraza16@gmail.com>
2026-04-25 09:45:56 +0500

    feat: add custom hook for social authentication using Clerk SSO

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, ScrollView } from 'react-native'
+
+const ChatsTab = () => {
+  return (
+    <ScrollView className='bg-surface'
+    contentInsetAdjustmentBehavior='automatic'>
+      <Text className='text-white'>ChatsTab</Text>
+    </ScrollView>
+  )
+}
+
+export default ChatsTab
---
+++ mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text, ScrollView } from 'react-native'
+
+const ProfileTab = () => {
+  return (
+    <ScrollView className='bg-surface'
+      contentInsetAdjustmentBehavior='automatic'>
+      <Text className='text-white'>ProfileTab</Text>
+    </ScrollView>
+  )
+}
+
+export default ProfileTab
